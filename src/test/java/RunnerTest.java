758b082982d6d33c6edb2ce22913a26efff1b2f0
AAC <mr.aranest@mail.ru>
2021-11-12 01:28:06 +0700

    with allure

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -2,15 +2,21 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"StepDefinition","Hooks"},
-        plugin = "pretty",
+        glue = {"StepDefinition", "Hooks"},
+        plugin = {"pretty", "html:target/cucumber-html-reports",
+                "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm",
+                "json:target/cucumber.json"},
         tags = "@auth or @check or @path or @creating"
 )
 
 public class RunnerTest {
 
 }
+
+
+
 
